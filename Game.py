daf39ddc13571f1cff7d366d4d9cd907647f6621
Leonn190 <euleonsoto@gmail.com>
2026-02-24 08:59:46 -0300

    Cria loop principal inicial do jogo (#1)

---
+++ Game.py
@@ -0,0 +1,56 @@
+import sys
+import pygame
+
+from Codigo.Cenas.CenaMenu import MenuLoop
+from Codigo.Cenas.CenaEstrategista import EstrategistaLoop
+from Codigo.Cenas.CenaBatalha import BatalhaLoop
+
+
+pygame.init()
+
+# Tela principal do jogo
+TELA_LARGURA = 1280
+TELA_ALTURA = 720
+TELA = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
+pygame.display.set_caption("TFT2")
+
+RELOGIO = pygame.time.Clock()
+
+CONFIG = {
+    "FPS": 60,
+    "Volume": 0.5,
+    "Mudo": False,
+    "MostrarFPS": False,
+}
+
+INFO = {
+    "Carregado": True,
+    "Alvo": "Menu",
+    "Round": 1,
+}
+
+# Dicionário que controla qual cena está ativa
+ESTADOS = {
+    "Rodando": True,
+    "Menu": True,
+    "Estrategista": False,
+    "Batalha": False,
+}
+
+
+while ESTADOS["Rodando"]:
+    if ESTADOS["Menu"]:
+        MenuLoop(TELA, RELOGIO, ESTADOS, CONFIG, INFO)
+
+    elif ESTADOS["Estrategista"]:
+        EstrategistaLoop(TELA, RELOGIO, ESTADOS, CONFIG, INFO)
+
+    elif ESTADOS["Batalha"]:
+        BatalhaLoop(TELA, RELOGIO, ESTADOS, CONFIG, INFO)
+
+    else:
+        # Segurança: sempre volta para o menu se nenhum estado estiver ativo
+        ESTADOS["Menu"] = True
+
+pygame.quit()
+sys.exit()
